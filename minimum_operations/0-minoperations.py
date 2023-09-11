97b7cd25e46a10336fbf105d2eae447d7df17b0b
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-09-11 11:12:30 +0000

    minimum operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """Minimum operations"""
 
+import math
 
 def minOperations(n):
     if n <= 1:
         return 0
 
     operations = 0
-    divisor = 2  # Start with the smallest prime divisor
-
-    while divisor <= math.sqrt(n):
-        while n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        divisor += 1
+    for i in range(2, int(math.sqrt(abs(n))) + 1):
+        while n % i == 0:
+            operations += i
+            n //= i
 
     if n > 1:
         operations += n
 
-    return operations    
+    return operations
