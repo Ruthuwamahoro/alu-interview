5dc624a3f4a155a5a191b397a69d044c1510c828
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-10-04 10:03:19 -0700

    finally

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -5,27 +5,26 @@
 
 
 def rain(walls):
-    """python module"""
-    if not walls:
+    """
+    Calculate how many square units of water will be retained between walls.
+    """
+    if not walls or len(walls) < 3:
         return 0
 
     n = len(walls)
     left_max = [0] * n
     right_max = [0] * n
-    water = 0
+    water_trapped = 0
 
-    # Initialize left_max array
     left_max[0] = walls[0]
     for i in range(1, n):
         left_max[i] = max(left_max[i - 1], walls[i])
 
-    # Initialize right_max array
     right_max[n - 1] = walls[n - 1]
     for i in range(n - 2, -1, -1):
         right_max[i] = max(right_max[i + 1], walls[i])
 
-    # Calculate water trapped at each position
     for i in range(n):
-        water += min(left_max[i], right_max[i]) - walls[i]
+        water_trapped += min(left_max[i], right_max[i]) - walls[i]
 
-    return water 
+    return water_trapped
